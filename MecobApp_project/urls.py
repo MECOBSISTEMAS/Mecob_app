96620d3cab70272010bd3c7849e54b5ea5287ff6
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-06-02 17:44:26 -0300

    First Commit

---
+++ MecobApp_project/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    #acesso a pagina de administração
+    path('admin/', admin.site.urls),
+    #acesso a pagina de api de autenticação do django-rest-framework
+    path('api-auth/', include('rest_framework.urls')),
+    #acesso aos apps instanciados pelo django
+    path('', include('Core_app.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
