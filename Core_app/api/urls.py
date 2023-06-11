467671fb1ee898be6c45466d2389cfcaeaf4bffe
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-06-11 14:16:43 -0300

    SimpleJWT instalado e configurado

--- Core_app/api/urls.py
+++ Core_app/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework import routers
 
 from . import viewsets
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 router = routers.DefaultRouter()
 router.register('pessoas', viewsets.PessoasModelViewSet, basename='pessoas')
@@ -13,4 +14,6 @@ router.register('contratos-comprador', viewsets.ContratosCompradorViewSet, basen
 router.register('consulta-juridico', viewsets.ConsultaJuridicoViewSet, basename='consulta-juridico')
 router.register('recuperacao-credito', viewsets.RecuperacaoCreditoViewSet, basename='recuperacao-credito')
 urlpatterns = [
+  path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
--- MecobApp_project/settings.py
+++ MecobApp_project/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     #? configuração da paginação por elementos
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
