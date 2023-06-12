3d84c0a16793b271547cdacc9830eea280f8b216
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-06-12 18:30:09 -0300

    Contrato Comprador email configurado

--- Core_app/api/urls.py
+++ Core_app/api/urls.py
@@ -12,10 +12,12 @@ router.register('eventos', viewsets.EventosModelViewSet, basename='eventos')
 router.register('contratos-vendedor', viewsets.ContratosVendedorViewSet, basename='contratos-vendedor')
 router.register(r'contratos-vendedor-email/seu-email-aqui', viewsets.ContratosVendedorEmailViewSet, basename='contratos-vendedor-email')
 router.register('contratos-comprador', viewsets.ContratosCompradorViewSet, basename='contratos-comprador')
+router.register('contratos-comprador-email/seu-email-aqui', viewsets.ContratosCompradorEmailViewSet, basename='contratos-comprador-email')
 router.register('consulta-juridico', viewsets.ConsultaJuridicoViewSet, basename='consulta-juridico')
 router.register('recuperacao-credito', viewsets.RecuperacaoCreditoViewSet, basename='recuperacao-credito')
 urlpatterns = [
   path('contratos-vendedor-email/<str:email>/', viewsets.ContratosVendedorEmailViewSet.as_view({'get': 'list'}), name='contratos-vendedor-email'),
+  path('contratos-comprador-email/<str:email>/', viewsets.ContratosCompradorEmailViewSet.as_view({'get': 'list'}), name='contratos-comprador-email'),
   path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
--- Core_app/api/viewsets.py
+++ Core_app/api/viewsets.py
@@ -48,27 +48,46 @@ class ContratosVendedorViewSet(viewsets.ViewSet):
         return Response(queryset_serialized.data)
     
 class ContratosVendedorEmailViewSet(viewsets.ViewSet):
+    contratos = Contratos.objects.none()
 
     def list(self, request, email):
         contratos_queryset = Contratos.objects.filter(
             vendedor=Pessoas.objects.get(email=email),
             status='confirmado'
         )
-        #.values('id','descricao', 'dt_contrato', 'vl_contrato','nu_parcelas', 'status', 'suspenso', 'vendedor', 'comprador', 'eventos')
+        
         contratos_serialized = ContratosModelSerializer(contratos_queryset, many=True).data
 
         for contrato in contratos_serialized:
             parcelas_queryset = ContratoParcelas.objects.filter(
-                contratos=contrato['id']).values(
-                'dt_credito', 'vl_parcela', 'id'
-            )
-            parcelas_serialized = ContratoParcelasModelSerializer(parcelas_queryset, many=True).data
-            contrato['parcelas'] = parcelas_serialized
+                contratos=contrato['id'])
+            contrato['parcelas'] = ContratoParcelasModelSerializer(parcelas_queryset, many=True).data 
             contrato['eventos'] = EventosModelSerializer(Eventos.objects.filter(id=contrato['eventos']), many=True).data
 
         queryset_serialized = {
             'contratos': contratos_serialized
         }
+        return Response(queryset_serialized)
+
+class ContratosCompradorEmailViewSet(viewsets.ViewSet):
+    def list(self, request, email):
+        contratos_queryset = Contratos.objects.filter(
+            comprador=Pessoas.objects.get(email=email),
+            status='confirmado'
+        )
+        contratos_serialized = ContratosModelSerializer(contratos_queryset, many=True).data
+        
+        
+        for contrato in contratos_serialized:
+            parcelas_queryset = ContratoParcelas.objects.filter(
+                contratos=contrato['id'])
+            contrato['parcelas'] = ContratoParcelasModelSerializer(parcelas_queryset, many=True).data
+            contrato['eventos'] = EventosModelSerializer(Eventos.objects.filter(id=contrato['eventos']), many=True).data
+            
+        queryset_serialized = {
+            'contratos': contratos_serialized
+		}
+            
         return Response(queryset_serialized)
     
 class ContratosCompradorViewSet(viewsets.ViewSet):
@@ -76,19 +95,21 @@ class ContratosCompradorViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
     contratos: Contratos.objects.none()
     def list(self, request):
-        contratos = Contratos.objects.filter(
+        self.contratos = Contratos.objects.filter(
             comprador=Pessoas.objects.get(email=request.user.username),
             status='confirmado'
         )
         queryset_serialized = {
-            'contratos': ContratosModelSerializer(contratos, many=True).data
+            'contratos': ContratosModelSerializer(self.contratos, many=True).data
         }
         return Response(queryset_serialized)
     
     def retrive(self, request, pk):
-        queryset = Contratos.objects.get(pk=pk)
+        queryset = self.contratos.get(pk=pk)
         queryset_serialized = ContratosModelSerializer(queryset)
         return Response(queryset_serialized.data)
+    
+
 
 class ConsultaJuridicoViewSet(viewsets.ViewSet):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
