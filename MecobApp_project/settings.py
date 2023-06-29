d410e2d7efc8e434cd2bfac37b536b771db031af
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-06-29 15:35:55 -0300

    Envio de email para reset de password

--- MecobApp_project/settings.py
+++ MecobApp_project/settings.py
@@ -157,9 +157,14 @@ CORS_ORIGIN_WHITELIST = (
 } """
 
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+#?usar o envio de email no prompt do proprio django para teste
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
--- MecobApp_project/urls.py
+++ MecobApp_project/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from dj_rest_auth.views import PasswordResetConfirmView, PasswordResetView
+
 urlpatterns = [
     #acesso a pagina de administração
     path('admin/', admin.site.urls),
@@ -11,6 +13,10 @@ urlpatterns = [
     #acesso aos apps instanciados pelo django
     path('', include('Core_app.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    #path('rest-auth/', include('rest_auth.urls')),
+    #path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('rest-auth/password/reset/', PasswordResetView.as_view(), name='password_reset'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
