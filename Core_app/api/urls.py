ed7af63b7e55d9c5d065e13b4c1671d1d1b2c8fb
Marcus Dagostini <marcus@stackoverbug.com>
2023-12-08 17:09:46 -0300

    Adicionando comentários no arquivo urls.py

--- Core_app/api/urls.py
+++ Core_app/api/urls.py
@@ -5,26 +5,70 @@ from . import viewsets
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 router = routers.DefaultRouter()
+
+#Com o atalho ctrl + clique em 'PessoasModelViewSet'
+#O endpoint /pessoas irá listas todas as pessoas de 10 em 10 por página
 router.register('pessoas', viewsets.PessoasModelViewSet, basename='pessoas')
+
+#Com o atalho ctrl + clique em 'ContratoParcelasModelViewSet'
+#O endpoint /contrato_parcelas retorna todos os contratos das parcelas de 10 em 10 por página
 router.register('contrato_parcelas', viewsets.ContratoParcelasModelViewSet, basename='contrato_parcelas')
+
+#O endpoint /contratos retorna todos os contratos de 10 em 10 por página
 router.register('contratos', viewsets.ContratosModelViewSet, basename='contratos')
+
+#O endpoint /eventos retorna todos os eventos de 10 em 10 por página
 router.register('eventos', viewsets.EventosModelViewSet, basename='eventos')
+
+#O endpoint /contratos-vendedor irá retornar todos os contratos da pessoa como vendedora
 router.register('contratos-vendedor', viewsets.ContratosVendedorViewSet, basename='contratos-vendedor')
+
+#O endpoint /contratos-vendedor-email/seu-email-aqui irá retornar todos os contratos de uma pessoa como vendedora passando seu email como parametro
 router.register(r'contratos-vendedor-email/seu-email-aqui', viewsets.ContratosVendedorEmailViewSet, basename='contratos-vendedor-email')
+
+#O endpoint /contratos-comprador irá retornar todos os contratos de uma pessoa como compradora
 router.register('contratos-comprador', viewsets.ContratosCompradorViewSet, basename='contratos-comprador')
+
+#O endpoint /contratos-comprador-email/seu-email-aqui  irá retornar todos os contratos de uma pessoa como compradora passando seu email como parametro
 router.register('contratos-comprador-email/seu-email-aqui', viewsets.ContratosCompradorEmailViewSet, basename='contratos-comprador-email')
+
+#O endpoint /consulta-juridico irá retornar todos os contratos da pessoa como vendedora em que o contrato está com o status acao_judicial
 router.register('consulta-juridico', viewsets.ConsultaJuridicoViewSet, basename='consulta-juridico')
+
+#O endpoint /recuperacao-credito retorna todos os contratos da pessoa como vendedora em que o contrato está com o status pendente
 router.register('recuperacao-credito', viewsets.RecuperacaoCreditoViewSet, basename='recuperacao-credito')
+
+#O endpoint /contratos-email-status/seu-email-aqui ira retornar todos os contratos da pessoa como vendedora e suas parcelas passando seu e-mail parametro
 router.register('contratos-email-status/seu-email-aqui', viewsets.ContratosEmailStatusViewSet, basename='contratos-email-status')
+
+#O endpoint /contratos-parcelas-vendedor-email-status/seu-email-aqui/status-do-contrato
 router.register('contratos-parcelas-vendedor-email-status/seu-email-aqui/status-do-contrato', viewsets.ContratosParcelasVendedorEmailStatusModelViewSet, basename='contratos-parcelas-vendedor-email-status')
+
+#O endpoint /contratos-parcelas-comprador-email-status/seu-email-aqui/status-do-contrato
 router.register('contratos-parcelas-comprador-email-status/seu-email-aqui/status-do-contrato', viewsets.ContratosParcelasCompradorEmailStatusModelViewSet, basename='contratos-parcelas-comprador-email-status')
+
+#O endpoint /contratos-vendedor-email-status/seu-email-aqui/status-do-contrato
 router.register('contratos-vendedor-email-status/seu-email-aqui/status-do-contrato', viewsets.ContratosVendedorEmailStatusViewSet, basename='contratos-vendedor-email-status')
+
+#O endpoint /contratos-comprador-email-status/seu-email-aqui/status-do-contrato
 router.register('contratos-comprador-email-status/seu-email-aqui/status-do-contrato', viewsets.ContratosCompradorEmailStatusViewSet, basename='contratos-comprador-email-status')
+
+#O endpoint /dashboard/seu-email-aqui
 router.register('dashboard/seu-email-aqui', viewsets.DashBoardViewSet, basename='dashboard')
+
+#O endpoint /registro-usuario/email/password
 router.register('registro-usuario/email/password', viewsets.RegistroUsuarioViewSet, basename='registro-usuario')
+
+#O endpoint /resetar-senha/email/senha
 router.register('resetar-senha/email/senha', viewsets.ResetarSenhaViewSet, basename='resetar-senha')
+
+#O endpoint /ContratosAllContratoParcelasAllViewSet
 router.register('ContratosAllContratoParcelasAllViewSet', viewsets.ContratosAllContratoParcelasAllViewSet, basename='ContratosAllContratoParcelasAllModelViewSet')
+
+#O endpoint /contratos-vendedor-email-quantidade/seu-email-aqui/quantidade/status
 router.register('contratos-vendedor-email-quantidade/seu-email-aqui/quantidade/status', viewsets.ContratosVendedorEmailQuantidadeViewSet, basename='contratos-vendedor-email-quantidade')
+
+#O endpoint /contratos-comprador-email-quantidade/seu-email-aqui/quantidade/status
 router.register('contratos-comprador-email-quantidade/seu-email-aqui/quantidade/status', viewsets.ContratosCompradorEmailQuantidadeViewSet, basename='contratos-comprador-email-quantidade')
 urlpatterns = [
   path('custom-login/', viewsets.CustomLoginView.as_view(), name='custom-login'),
